b860b3f7672daff7e327fa846cb22238d578cc2c
Georgy Gusev <goosebumpsmail2@gmail.com>
2021-01-02 13:51:00 +0300

    change message send on failure in systemd service, clear curl output

--- bin/fatal_error_action.sh
+++ bin/fatal_error_action.sh
@@ -3,4 +3,4 @@
 #set msg from param
 msg=$1
 
-curl "https://api.telegram.org/bot863176881:AAHBJ2IUoNAIkxv9fLKpOfQop5eQe9p68gk/sendMessage?chat_id=304165806&text=Twich%20project%20error: ${msg}"
+curl "https://api.telegram.org/bot863176881:AAHBJ2IUoNAIkxv9fLKpOfQop5eQe9p68gk/sendMessage?chat_id=304165806&text=Twich%20project%20error: ${msg}" > /dev/null/
