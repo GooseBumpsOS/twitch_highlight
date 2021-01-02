1c110cb06395a021bd8f9639e81f69cc023b7c77
Georgy Gusev <goosebumpsmail2@gmail.com>
2021-01-02 13:32:49 +0300

    add logger add fatal error notify script

---
+++ bin/fatal_error_action.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#set msg from param
+msg=$1
+
+curl "https://api.telegram.org/bot863176881:AAHBJ2IUoNAIkxv9fLKpOfQop5eQe9p68gk/sendMessage?chat_id=304165806&text=Twich%20project%20error: ${msg}"
--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
